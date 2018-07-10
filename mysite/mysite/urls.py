b24b137009f152ae2789e819bf2f52b2a570f507
rosso4400 <aleksey_shusharin@mail.ru>
2018-07-10 11:47:16 +0300

    raw authentification

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,3 +20,7 @@ urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
+#Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -27,3 +27,4 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
